5211e745976ef77ce2341698a1943b0622353fcc
Muhammed khaled <muhammmed.k95@gmail.com>
2020-10-30 08:19:55 +0200

    remove  warnings

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -24,11 +24,11 @@ function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
                     <strong>{price}</strong>
                 </p>
                 <div className="checkoutProduct__rating">
-                    {/* {Array(rating)
+                    {Array(rating)
                     .fill()
                     .map((_, i) => (
-                        <p>🌟</p>
-                    ))} */}
+                        <p> <span></span> 🌟</p>
+                    ))}
                 </div>
                 {!hideButton && (
                     <button onClick={removeFromBasket}>Remove from Basket</button>
